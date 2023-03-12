c0a7745232d6c0456194c8a45100aada480f86bf
Zane Helton <zane99@me.com>
2023-03-12 12:02:13 -0400

    Try and fix scheduling bug

--- src/modules/LiveChecker.js
+++ src/modules/LiveChecker.js
@@ -10,6 +10,19 @@ var userAgents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/110.0.1587.69",
 ];
 
+let checkTask;
+
+const createCheckTask = () => {
+    checkTask = nodeCron.schedule(`*/${interval} * * * *`, async () => {
+        console.log("Checking if Sam is live");
+
+        await isSamLive();
+    });
+};
+
+// first time init
+createCheckTask();
+
 const isSamLive = async () => {
     const response = await fetch("https://www.youtube.com/@sam/streams", {
         headers: {
@@ -30,6 +43,7 @@ const isSamLive = async () => {
 
         checkTask.stop();
         interval = 59;
+        createCheckTask();
         checkTask.start();
 
         if (client) {
@@ -49,6 +63,7 @@ const isSamLive = async () => {
         console.log("Stream must have ended. Checking again in 6 minutes...");
         checkTask.stop();
         interval = 3;
+        createCheckTask();
         checkTask.start();
     } else {
         console.log("No updates...");
@@ -56,12 +71,6 @@ const isSamLive = async () => {
 
     return false;
 };
-
-const checkTask = nodeCron.schedule(`*/${interval} * * * *`, async () => {
-    console.log("Checking if Sam is live");
-
-    await isSamLive();
-});
 
 const setClient = (newClient) => {
     client = newClient;
